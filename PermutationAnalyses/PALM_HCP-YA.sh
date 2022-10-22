eda23b19644c6889e88114cd79d642330a4dc649
Logan Williams <61651199+lzjwilliams@users.noreply.github.com>
2022-10-22 14:07:36 +0100

    Update and rename PALM_HCP-YA to PALM_HCP-YA.sh

--- PermutationAnalyses/PALM_HCP-YA.sh
+++ PermutationAnalyses/PALM_HCP-YA.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 palm -i /data/Data/asymmetry/Nature_Human_Behaviour/merged_1110_sulc_asymmetry_smoothed_FINAL.shape.gii \
      -m /data/Data/dHCP_1/dhcp_3rd_release_atlas/week-40_hemi-left_space-dhcpSym_dens-32k_desc-medialwallsymm_mask_ero.HCP.shape.gii \
      -i /data/Data/asymmetry/Nature_Human_Behaviour/merged_1110_pial_corr_va_asymmetry_smoothed_FINAL.shape.gii \
