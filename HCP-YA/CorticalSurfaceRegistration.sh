1862f2fcca66f3fe6b7810848fc4dbf26525dbf8
Logan Williams <61651199+lzjwilliams@users.noreply.github.com>
2022-10-22 15:58:03 +0100

    Update CorticalSurfaceRegistration.sh

--- HCP-YA/CorticalSurfaceRegistration.sh
+++ HCP-YA/CorticalSurfaceRegistration.sh
@@ -7,12 +7,12 @@ for Hemisphere in L R ; do
 
 echo ${Subject} ${Hemisphere}
 
-msm --inmesh=/data/HCP/${Subject}/T1w/Native/100206.${Hemisphere}.sphere.rot.native.surf.gii \
+msm --inmesh=/data/HCP/${Subject}/MNINonLinear/Native/${Subject}.${Hemisphere}.sphere.rot.native.surf.gii \
     --refmesh=/data/CorticalAsymmetry/Templates/HCP-YA/${Hemisphere}.sphere.32k_fs_LR.surf.gii \
-    --indata=/data/HCP/${Subject}/T1w/Native/100206.${Hemisphere}.sulc.native.shape.gii \ 
+    --indata=/data/HCP/${Subject}/MNINonLinear/Native/${Subject}.${Hemisphere}.sulc.native.shape.gii \ 
     --refdata=/data/CorticalAsymmetry/Templates/HCP-YA/MSMSulc.refsulc.${Hemisphere}.LR.32k_fs_LR.shape.gii \
     --conf=/data/CorticalAsymmetry/Configs/MSMStrain_HCPCorticalAsymmetry \
-    -o /data/CorticalAsymmetry/HCPAsymmetry/${Subject}/T1w/Native/100206.L.MSMstrain. 
+    -o /data/CorticalAsymmetry/HCPAsymmetry/${Subject}/${Subject}.L.MSMstrain. 
 
 done
 
