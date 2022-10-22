bb5ed2541873fca9ec8f7c13aed61242d53ae0c8
Logan Williams <61651199+lzjwilliams@users.noreply.github.com>
2022-10-22 22:00:25 +0100

    Create CorticalSurfaceRegistration.sh

---
+++ dHCP_HCP-YA/CorticalSurfaceRegistration.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+InMesh=/data/CorticalAsymmetry/Templates/dHCP/week-40_hemi-left_space-dhcpSym_dens-32k_sphere.surf.gii 
+RefMesh=/data/CorticalAsymmetry/Templates/HCP-YA/L.sphere.32k_fs_LR.surf.gii 
+InData=/data/CorticalAsymmetry/Templates/dHCP/week-40_hemi-LR_space-dhcpSym_dens-32k_sulc.shape.gii
+RefData=/data/CorticalAsymmetry/Templates/HCP-YA/MSMSulc.refsulc.L.LR.32k_fs_LR.shape.gii
+Out=/data/CorticalAsymmetry/dHCP_HCP-YA/dHCP_HCP-YA.MSMStrain
+Config=/data/CorticalAsymmetry/Configs/config_subject_to_40_week_template_3rd_release
+
+msm --inmesh=${InMesh}
+    --refmesh=${RefMesh}
+    --indata=${InData}
+    --refdata=${RefData}
+    --out=${Out}
+    --conf=${Config}
