b8e070db56df70240f6532333a3c1f238f09dfe9
Logan Williams <61651199+lzjwilliams@users.noreply.github.com>
2022-10-22 15:55:36 +0100

    Add files via upload

---
+++ HCP-YA/CorticalSurfaceRegistration.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+
+for Subject in $( cat </data/CorticalAsymmetry/HCPAsymmetryList.txt) ; do 
+
+for Hemisphere in L R ; do 
+
+echo ${Subject} ${Hemisphere}
+
+msm --inmesh=/data/HCP/${Subject}/T1w/Native/100206.${Hemisphere}.sphere.rot.native.surf.gii \
+    --refmesh=/data/CorticalAsymmetry/Templates/HCP-YA/${Hemisphere}.sphere.32k_fs_LR.surf.gii \
+    --indata=/data/HCP/${Subject}/T1w/Native/100206.${Hemisphere}.sulc.native.shape.gii \ 
+    --refdata=/data/CorticalAsymmetry/Templates/HCP-YA/MSMSulc.refsulc.${Hemisphere}.LR.32k_fs_LR.shape.gii \
+    --conf=/data/CorticalAsymmetry/Configs/MSMStrain_HCPCorticalAsymmetry \
+    -o /data/CorticalAsymmetry/HCPAsymmetry/${Subject}/T1w/Native/100206.L.MSMstrain. 
+
+done
+
